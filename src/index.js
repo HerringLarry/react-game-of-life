f50ec122b016fcd7e04accbbac44eb736050777b
William Newman <william.newman94@gmail.com>
2018-01-23 17:45:33 -0500

    Created beginning of grid

--- src/index.js
+++ src/index.js
@@ -1,7 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+class Box extends React.Component{
+	render(){
+		return(<div>hello</div>
+			)
+	}
 
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Grid extends React.Component{
+	render(){
+		const width = this.props.cols * 14;
+		var rowsArr = [];
+		var boxClass = "";
+		for(var i = 0; i < this.props.rows; i++){
+			for(var j = 0; j < this.props.cols; j++){
+				let boxId = i + "_" + j;
+				boxClass = this.props.gridBool[i][j] ? "Box on" : "Box off";
+				rowsArr.push(
+					<Box 
+						boxClass = {boxClass}
+						key = {boxId}
+						row = {i}
+						col = {j}
+						//selectBox {this.props.selectBox}
+
+					/>
+					)
+			}
+		}
+		return(
+			<div className = "grid" style = {{width : width}}>
+				{rowsArr}
+			</div>
+		);
+	}
+}
+
+class Main extends React.Component{
+	constructor(){
+		super();
+		this.speed = 100;
+		this.rows = 50;
+		this.cols = 40;
+		this.state = {
+			generation: 0,
+			gridBool: Array(this.rows).fill(Array(this.colss).fill(false))﻿
+		}
+	}
+	render(){
+		return(
+			<Grid
+				gridBool={this.state.gridBool}
+				rows = {this.rows}
+				cols = {this.cols}
+				//selectBox = {this.selectBox}
+			/>
+
+			);
+
+	}
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
